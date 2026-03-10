150e241a04b18be332c91eb3fcdcc7d05a9af560
Joakim <joakim.for.github@gmail.com>
2026-03-10 16:30:58 +0100

    Vector.h Minor fixes

--- src/objects/Vector.h
+++ src/objects/Vector.h
@@ -13,22 +13,22 @@ namespace Objects {
     public:
         Vector() {
             n_ = 1;
-            data_ = new double[1];
-        };
+            data_ = new double[n_];
+            for (int i = 0; i < n_; i++) data_[i] = 0;
+        }
+
         explicit Vector(const int n) : n_(n) {
             data_ = new double[n];
-            for (int i = 0; i < n; i++) {
-                data_[i] = 0;
-            }
+            for (int i = 0; i < n; i++) data_[i] = 0;
         }
+
         explicit Vector(Vector &other) {
             n_ = other.n_;
             data_ = new double[n_];
-            for (int i = 0; i < n_; i++) {
-                data_[i] = other[i];
-            }
+            for (int i = 0; i < n_; i++) data_[i] = other[i];
         }
-        Vector &operator=(const Vector &other) noexcept {
+
+        Vector &operator=(const Vector &other) noexcept { // NOLINT
             n_ = other.n_;
 
             if (other.data_ == nullptr) return *this;
@@ -40,6 +40,7 @@ namespace Objects {
 
             return *this;
         }
+
         ~Vector() {
             delete data_;
         }
